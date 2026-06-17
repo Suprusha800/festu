0fcda59e3534f873992a977491490f0987385f8b
supru <suprun78003@gmail.com>
2026-06-17 19:34:20 +1000

    add media

---
+++ src/lucide-react.d.ts
@@ -0,0 +1,27 @@
+declare module 'lucide-react' {
+  import type { FC, SVGProps } from 'react';
+
+  export type LucideIcon = FC<SVGProps<SVGSVGElement> & { size?: number | string }>;
+
+  export const Accessibility: LucideIcon;
+  export const ArrowUpRight: LucideIcon;
+  export const Building2: LucideIcon;
+  export const CheckCircle2: LucideIcon;
+  export const CircleHelp: LucideIcon;
+  export const ClipboardCheck: LucideIcon;
+  export const Contrast: LucideIcon;
+  export const Eye: LucideIcon;
+  export const FileText: LucideIcon;
+  export const Headphones: LucideIcon;
+  export const Mail: LucideIcon;
+  export const Menu: LucideIcon;
+  export const MapPinned: LucideIcon;
+  export const MousePointerClick: LucideIcon;
+  export const Pause: LucideIcon;
+  export const Phone: LucideIcon;
+  export const Play: LucideIcon;
+  export const Route: LucideIcon;
+  export const TextCursorInput: LucideIcon;
+  export const X: LucideIcon;
+  export const ZoomIn: LucideIcon;
+}
